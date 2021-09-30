b81622b1d7c46c3847d3859e84e26ba37ef2a0f6
rubysaltbush <stephenseruby@gmail.com>
2021-10-01 08:46:27 +1000

    Updating figure 5 species range graph

--- scripts/analyses/species_regressions.R
+++ scripts/analyses/species_regressions.R
@@ -1,7 +1,7 @@
-#species regressions
-#define regressions to do
+# species regressions
+# define regressions to do
 regressions_todo <- list(
-  #eoo
+  # eoo
   lmeoo_months = list(
     xdata = species_all$monthsCount,
     ydata = species_all$eoo_aus,
@@ -11,10 +11,10 @@ regressions_todo <- list(
   )
 )
 
-#prep empty list for results and df for result table
+# prep empty list for results and df for result table
 speciesregs <- data.frame()
 speciesregressions <- list()
-#loop over regressions_todo, perform linear regression and output graphs
+# loop over regressions_todo, perform linear regression and output graphs
 for (regression_name in names(regressions_todo)) {
   todo <- regressions_todo[[regression_name]]
   speciesregressions[[regression_name]] <- do_regression(
@@ -32,13 +32,17 @@ for (regression_name in names(regressions_todo)) {
 
 write_csv(speciesregs, "data_output/species_regression_results.csv")
 
-#graph of monthsCount versus extent of occurrence (EOO)
-ggplot(species_all, aes(y = eoo_aus, x = monthsCount)) +
-  geom_point() +
+# add extra column to species_all to graph EOO with smaller numbers
+species_all <- species_all %>%
+  dplyr::mutate(eoo_aus_mill = eoo_aus/1000000)
+
+# graph of monthsCount versus extent of occurrence (EOO)
+ggplot(species_all, aes(y = eoo_aus_mill, x = monthsCount)) +
+  geom_point(aes(alpha = 0.5)) +
   geom_smooth(method = "lm", se = FALSE, colour = "red") +
-  theme_pubr() +
+  theme_pubr(legend = "none") +
   scale_x_continuous(breaks = seq(1, 12, by=1), limits=c(1,12)) +
-  ylab("Extent of Occurrence (km²)") +
+  ylab("Extent of Occurrence (million km²)") +
   xlab("Species length of flowering period (months)") +
   theme(axis.title = element_text(size = 14), axis.text = element_text(size = 14)) +
   labs(title = paste("R² = ", signif(summary(speciesregressions$lmeoo_months)$r.squared, 2),
